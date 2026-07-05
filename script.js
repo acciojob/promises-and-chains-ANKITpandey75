c910ac45d4c7a0b53f00cec86b9dfff1e3dae86f
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-07-05 21:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value.trim();
+
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((message) => {
+      alert(message);
+    })
+    .catch((message) => {
+      alert(message);
+    });
+});//your JS code here. If required.
